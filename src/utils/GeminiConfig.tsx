cd3d38149b662c7b33311253667ed97ed064644a
addahadi <addajs48@gmail.com>
2025-01-21 20:11:00 +0100

    Add .env to .gitignore

--- src/utils/GeminiConfig.tsx
+++ src/utils/GeminiConfig.tsx
@@ -2,7 +2,7 @@ import  {GoogleGenerativeAI} from "@google/generative-ai"
 
 
 
-const genAI = new GoogleGenerativeAI("AIzaSyBO3GPet4N0deLoXGLHsvsdQvS_AYye7zM");
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 
